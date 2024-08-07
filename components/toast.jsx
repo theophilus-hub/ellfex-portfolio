8011281d61d8c3d0acb3862601a261bf9c4c877d
web design <wdesign589@gmail.com>
2024-08-07 09:14:05 +0100

    FINALV2 commit

--- components/toast.jsx
+++ components/toast.jsx
@@ -23,13 +23,15 @@ function Toast() {
     <div className="w-full flex md:justify-end justify-center items-center fixed top-2 md:right-2 ">
     <div className='rounded-2xl bg-notblack w-min whitespace-nowrap p-4 space-y-2 flex flex-col justify-center items-center border border-notnotblack'>
       <h1 className='font-semibold text-white text-base md:text-xl'>JOIN BUILDERS HUB COMMUNITY NOW</h1>
-      
-      <a href="https://t.me/EllfexBuildersHub" className="flex flex-row border border-notblack rounded-[10px] w-min whitespace-nowrap py-2 px-2 my-4 bg-blue-800">
-               
+      <button>
+           <a href="https://t.me/EllfexBuildersHub" className="flex flex-row border border-notblack rounded-[10px] w-min whitespace-nowrap py-2 px-2 my-4 bg-blue-800">
+              
         <p className="text-sm font-semibold  text-white">
           Join Now
         </p>
-      </a>
+      </a> 
+            </button> 
+      
     </div>
   </div>
   )
